87962eac889bea72c83fe65ae26b5c693178e401
afolabidave <afolabidave9@gmail.com>
2022-09-08 14:22:54 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  * main - Printing
  *
  * Return: Always 0 (sucessful)
- */
+ * /
 int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+      puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
