003464f9767bd4e941eabd560757fac0edb21be2
afolabidave <afolabidave9@gmail.com>
2022-09-08 14:55:40 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Serves as the entry point for the program
  *
- * Return: By default returns zero if no erros otherwise returns non-zero value.
+ * Return: By default returns zero if no erros otherwise return non-zero value.
  */
 int main(void)
 {
- puts("\"programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
